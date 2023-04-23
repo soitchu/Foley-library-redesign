aa5887e9a9dcf0d1d05b73e4db741cc4685c0acb
soitchu <suyash4204@gmail.com>
2023-04-22 23:26:37 -0700

    Added other stuff

--- assets/index.js
+++ assets/index.js
@@ -4,6 +4,16 @@ const placeholders = [
     "Search by instructor, course number, item title, or keyword",
     "Search by journal, magazine, or newspaper title"
 ];
+const menuItems = document.querySelectorAll(".menuItem");
+const spotlightDOM = document.querySelector(".spotlight");
+
+function closeAllMenu() {
+    for (const item of menuItems) {
+        item.classList.remove("open");
+        item.setAttribute("open", "false");
+    }
+}
+
 for (const tab of searchTabs) {
     tab.addEventListener("click", function () {
         let count = 0;
@@ -19,3 +29,43 @@ for (const tab of searchTabs) {
         }
     });
 }
+
+for (const item of menuItems) {
+    const expandedMenu = item.querySelector(".menuExpanded");
+    const expandedClose = item.querySelector(".closeMenuExpanded");
+
+    if (expandedMenu) {
+        expandedMenu.addEventListener("click", function (event) {
+            event.preventDefault();
+            event.stopPropagation();
+        });
+    }
+
+    if (expandedClose) {
+        expandedClose.addEventListener("click", function (event) {
+            item.classList.remove("open");
+            spotlightDOM.style.display = "none";
+        });
+    }
+
+    item.addEventListener("click", function () {
+        closeAllMenu();
+        console.log(this.getAttribute("open"));
+        if (this.getAttribute("open") === "true") {
+            spotlightDOM.style.display = "none";
+            this.classList.remove("open");
+            this.setAttribute("open", "false");
+        } else {
+            spotlightDOM.style.display = "block";
+            this.classList.add("open");
+            this.setAttribute("open", "true");
+        }
+    });
+
+
+}
+
+spotlightDOM.addEventListener("click", function () {
+    closeAllMenu();
+    this.style.display = "none";
+});
